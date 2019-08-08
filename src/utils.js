9cc62ed4e40eae0e4e228ff41d511da68c1e8e08
yongsk0066 <yongsk0066@naver.com>
2019-08-08 16:35:41 +0900

    sendMail Function

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,36 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({path: path.resolve(__dirname, ".env")});
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+import { FieldConfigUtils } from "prisma-generate-schema/dist/src/generator/generator";
+
 
 
 export const generateSecret = () => {
     const randomNumber = Math.floor(Math.random() * adjectives.length);
     return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = (email) => {
+    const options = {
+        auth: {
+            api_user: process.env.SENDGRID_USERNAME,
+            api_key: process.env.SENDGRID_PASSWORD
+        }
+    };
+    const client = nodemailer.createTransport(sgTransport(options));
+    return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+    const email = {
+        from: "example@example.com",
+        to: address,
+        subject: "Login Secret for Prismagram",
+        html: `hello! Your login Secret it ${secret}.<br/>Copy paste on the page to login`
+    };
+    return sendMail(email);
 };
